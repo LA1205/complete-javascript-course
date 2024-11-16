954d84934dc5466d9eaf073a1c2b7bdc4695b5e8
LA1205 <zhengfangyuan@outlook.com>
2024-11-16 21:22:09 +0800

    add challange 1 code

---
+++ 01-Fundamentals-Part-1/final/code_challange1.js
@@ -0,0 +1,8 @@
+const Markmass = 78;
+const Markheight = 1.69;
+const Johnmass = 92;
+const Johnheight = 1.95;
+let BMIMark = Markmass / Markheight ** 2;
+let BMIJohn = Johnmass / Johnheight ** 2;
+let markHigherBMI = BMIMark > BMIJohn;
+console.log("Mark has heigher BMI is " + markHigherBMI + ".");
